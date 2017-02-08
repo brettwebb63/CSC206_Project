b7222bb6ddf87f7c5066cbffdbbd340e7a39859e
Scott Madeira <scottmadeira@gmail.com>
2017-02-07 22:10:10 -0500

    Added templates and SQL Classes

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+// Load all application files and configurations
+require($_SERVER['DOCUMENT_ROOT'].'/includes/application_includes.php');
+
+// Include the HTML layout class
+require_once(WS_TEMPLATES . 'Layout.php');
+
+// Connect to the database
+$db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Run a simple query that will be rendered below
+$sql = 'select * from pages';
+$res = $db->query($sql);
+
+
+// Generate the HTML for the top of the page
+Layout::pageTop('CSC206 Project');
+
+/**
+ *
+ * This implementation mixes html and php code to enter data
+ * It's a simple implementation but it works
+ *
+ */
+?>
+    <div class="container top25">
+        <div class="col-md-6">
+            <section class="content">
+                <h1>Pages List - Simple</h1>
+                <p>This imeplemntation builds only the table contents from the results set.
+                It is a mixture of HTML and PHP code so it is a bit messy and not the best implementation.</p>
+                <table class="table table-hover">
+                   <tr>
+                       <th>id</th>
+                       <th>name</th>
+                       <th>description</th>
+                       <th>created_at</th>
+                       <th>updated_at</th>
+                   </tr>
+<?php
+                while ($row = $res->fetch()){
+                    echo '<tr>';
+                        echo '<td>' . $row['id'] . '</td>';
+                        echo '<td>' . $row['name'] . '</td>';
+                        echo '<td>' . $row['description'] . '</td>';
+                        echo '<td>' . date('m/d/Y H:i:s', strtotime($row['created_at'])) . '</td>';
+                        echo '<td>' . $row['updated_at'] . '</td>';
+                    echo '</tr>';
+                }
+?>
+                </table>
+            </section>
+        </div>
+<?php
+/**
+ *
+ * This implementation abstracts out the table creation
+ *
+ * The implementation is a bit more complicated but notice how simple it is
+ * to render the table on your page.  This is the preferred way to do it.
+ *
+ */
+?>
+        <div class="col-md-6">
+            <section class="content">
+                <h1>Pages List - Abstracted</h1>
+                <p>This imeplementation builds full table from the results set. This implementation is better
+                    because there is no mixture of HTML on the page.  The method merges the data</p>
+<?php
+                $table = Layout::buildTable($res->fetchAll());
+                echo $table;
+?>
+            </section>
+        </div>
+    </div>
+
+<?php
+
+
+// Generate the page footer
+Layout::pageBottom();
---
+++ templates/Layout.php
@@ -0,0 +1,167 @@
+<?php
+
+class layout
+{
+
+    public static function pageTop($title)
+    {
+        $appcss = WS_CSS . 'app.css';
+        echo <<<pagetop
+    <!doctype html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>$title</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="author" content="Scott Madeira">
+        <meta name="description" content="Everything you wanted to know about web programming">
+        <meta name="keywords" content="xampp, php, mysql, html, web programming">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--[if lte IE 9]>
+            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        
+        <!-- Embed fonts for the page -->
+        <link href='https://fonts.googleapis.com/css?family=Miriam+Libre:400,700|Source+Sans+Pro:200,400,700,600,400italic,700italic' rel='stylesheet' type='text/css'>
+        
+        <!-- Latest compiled and minified CSS - get from getbootstrap.com-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+        <link rel="stylesheet" href="$appcss">
+    
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+    </head>
+    <body class="home language-php">
+        
+        <header class="navbar navbar-default navbar-fixed-top topnav" role="banner">
+            <div class="container topnav">
+                <nav role="navigation">
+                    <div class="container-fluid">
+                      <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                              <span class="sr-only">Toggle navigation</span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                              <span class="icon-bar"></span>
+                            </button>
+                                <a class="navbar-brand topnav" href="/">CSC206 Project</a>
+                        </div>
+        
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="navbar-collapse-1">						  
+                            <ul class="nav navbar-nav  navbar-right">
+                                
+                                <li><a href="/about.php">About</a></li>
+                                                        
+                                <li><a href="/events.phps">Events</a></li>	                       	                       
+                                
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin<span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Posts</a></li>
+                                        <li><a href="#">Users</a></li>
+                                        <li><a href="#">Events</a></li>                                    
+                                    </ul>
+                                </li>
+                                <li><a href="/login.php"><i class="fa fa-sign-in fa-fw"></i> Login</a></li>	                       
+                            </ul>						
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div>		
+        </header>
+            <div class="intro-header">
+	   	<h2>Web Development</h2>
+	</div>
+pagetop;
+
+    }
+
+
+    public static function pageBottom()
+    {
+
+        echo <<<pagebottom
+        <!-- Footer -->
+	<div class="site-footer">
+	    <div class="row">
+	        
+	    	<p>All content copyright Scott Madeira 2017</p>
+	        
+	    </div>
+	</div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
+</body>
+</html>
+pagebottom;
+
+
+    }
+
+    /**
+     * This method will take a 2-dimensional array and create a table.
+     * The header columns are derived from the keys of the row data
+     *
+     * @param $data
+     * @return string
+     */
+    public function buildTable($data)
+    {
+        // Start building the table
+        $table = '<table class="table table-hover">';
+
+        // Create the table header row
+        $header = '<tr>';
+
+        foreach($data[0] as $key => $cell){
+            $header .= '<th>' . $key . '</th>';
+        }
+        $header .= '</tr>';
+
+        // Add the header to the table
+        $table .= $header;
+
+        // Build the table rows
+        $rowHTML = '';
+
+        // Loop through each row of data and build a row
+        foreach($data as $row){
+            $rowHTML .= '<tr>';
+
+            // Loop through each cell and create the cells
+            foreach($row as $cell){
+                $rowHTML .= '<td>' . $cell . '</td>';
+            }
+            $rowHTML .= '</tr>';
+        }
+
+        // Add the rows to the table
+        $table .= $rowHTML;
+
+        // Close out the table
+        $table .= '</table>';
+
+        return $table;
+    }
+
+
+
+}
