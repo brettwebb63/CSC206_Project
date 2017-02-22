bbfaf8d90e9a2c92dd6948ee227898439335621a
Scott Madeira <scottmadeira@gmail.com>
2017-02-22 14:50:44 -0500

    Completed createPost skeleton to be a working page

--- templates/News.php
+++ templates/News.php
@@ -25,12 +25,14 @@ class news
     {
         $title = $data['title'];
         $content = $data['content'];
+        $startDate = date('m/d/Y', strtotime($data['startDate']));
+        $endDate = date('m/d/Y', strtotime($data['endDate']));
        // $author = $data['firstname'] . ' ' . $data['lastname'];
 
         echo <<<story
         <div class="top10">
             <h2>$title</h2>
-            <h5>Reported by </h5>
+            <h5>Display from  $startDate until $endDate</h5>
             <p>$content</p>
         </div>        
 story;
